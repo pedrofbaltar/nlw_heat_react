b09fda82abe60df41df78629c226fc83e9f44756
Pedro <pfbaltariead@gmail.com>
2021-11-02 16:10:47 -0300

    Final commit

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,7 +1,8 @@
+import logoImg from "../../assets/logo.svg";
 import { useEffect, useState } from "react";
+import io from "socket.io-client";
 import { api } from "../../services/api";
 
-import logoImg from "../../assets/logo.svg";
 import styles from "./styles.module.scss";
 
 type Message = {
@@ -13,24 +14,44 @@ type Message = {
   };
 };
 
+const messagesQueue: Message[] = [];
+
+const socket = io("http://localhost:4000");
+
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
-    api.get<Message[]>("messages/last").then((res) => {
-      setMessages(res.data);
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    api.get<Message[]>("messages/last").then((response) => {
+      setMessages(response.data);
     });
   }, []);
 
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
+
       <ul className={styles.messageList}>
         {messages.map((message) => {
           return (
             <li key={message.id} className={styles.message}>
               <p className={styles.messageContent}>{message.text}</p>
-
               <div className={styles.messageUser}>
                 <div className={styles.userImage}>
                   <img src={message.user.avatar_url} alt={message.user.name} />
